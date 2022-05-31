3c6d1fc41628bb4990e7dbec854d26d0ff21db24
shuizeng Yu <shuizengyu@91jkys.com>
2022-05-31 13:20:02 +0800

    update Theme color

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const CracoAntDesignPlugin = require('craco-antd');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: {
+          '@primary-color': '#FA7436',
+        },
+      },
+    },
+  ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Button } from 'antd';
-import './App.css';
+import './App.less';
 
 const App: FC = () => (
   <div className="App">
